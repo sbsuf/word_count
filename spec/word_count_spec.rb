f84ea58cac75931de779fa1dbc028015b17e4af3
sbsuf <justinjxu@yahoo.com>
2016-12-04 11:41:56 -0800

    fix word_count_spec

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -6,7 +6,7 @@ describe('String#word_count') do
   it('take users input then count the most common word') do
     expect("world".word_count("world")).to(eq(1))
   end
-  it("it checks string for word count") do
-    expect("Dog like human and human likes dog".word_count("dog")).to(eq(2))
+  it("it checks string for word count regardeless of capitalization") do
+    expect("Dog like human and human likes dOg".word_count("DOg")).to(eq(2))
   end
 end
